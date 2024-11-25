fe6ebd654a7be5a22c7e5cd8f446932aaf481c45
unknown <ezequielafeita@gmail.com>
2024-11-24 22:06:52 -0300

    Form

---
+++ main/src/assets/components/Query.jsx
@@ -0,0 +1,15 @@
+
+
+const Query = () =>{
+    return (
+        <div className="query-form">
+            <form action="">
+                <textarea name="Query" id="">Escribido</textarea>
+                <button className="query-button">Enviar</button>
+            </form>
+        </div>
+
+    )
+}
+
+export default Query
