5cba29aeb2e843fbed7369aa57cde7d1405878a8
Zique <ezequielafeita@gmail.com>
2024-12-09 23:07:34 -0300

    Data

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,8 @@ import mysql2 from "mysql2/promise"
 const CreateConnection = async(data) =>{
     // const {host,user,password} = data 
     const Connection = await mysql2.createConnection({
-        host:"localhost",
-        user:"root",
+        host:data.host,
+        user:data.user,
         password:data.password
     })
     return Connection
