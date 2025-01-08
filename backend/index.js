09fd5044f26cdb9d9cdc68f0d6668fc76da6bbe1
Zique <ezequielafeita@gmail.com>
2025-01-08 18:07:12 -0300

    Params

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,70 @@ App.post("/",async (req,res)=>{
         //----------------------
         //Si existe una consulta realizarla
         if (req.body.query){
+            await resp.query('USE ?', req.params.database)
+            const result = await resp.query(req.body.query)
+            info.result = result
+        }
+        //Mostrar todas las bases de datos existentes
+
+        
+    }catch(error){
+        console.log("error",error)
+        if (error.errorno == 1045){
+            res.clearCookie("user")
+        }
+        res.render("login.ejs",{
+            title:"login",
+            info:info
+        })
+    }
+
+
+    res.render("home.ejs",{
+        title:"home",
+        info:info
+    })
+})
+
+App.post("/:db",async (req,res)=>{
+    console.log(req.params.db)
+    //Renderizado completo de la página
+    const cookie = req.headers.cookie ? JSON.parse(decodeURIComponent(req.headers.cookie.split("=")[1]).slice(2,-7)) : false;
+    let info = {}
+    if (req.body.session == "logout"){
+        res.clearCookie("user")
+    }
+    try{
+        const resp = await db(cookie ? cookie : req.body)
+        //Crear la información necesaria para el renderizado
+        const query = await resp.query(`SELECT TABLE_NAME,TABLE_SCHEMA FROM INFORMATION_SCHEMA.TABLES`  )
+        const map = []
+        await query[0].map((el)=>{
+            const indice = map.findIndex((e) => e.database == el.TABLE_SCHEMA);
+            if (indice == -1){
+                map.push({
+                    "database":el.TABLE_SCHEMA,
+                    "tables":[`${el.TABLE_NAME}`]
+                })   
+            }else{
+                map[indice].tables.push(el.TABLE_NAME)
+            }})
+        info.dbs = map
+
+        //Crear Cookie Si No Existe
+        if (!cookie){
+            console.log("no hay cookie")
+            const session = {
+                user: req.body.user,
+                password: req.body.password,
+                database: req.body.database
+            }
+            res.cookie("user",session)
+        }
+        //----------------------
+        //Si existe una consulta realizarla
+        if (req.body.query){
+            await resp.query('USE ?', req.params.database)
             const result = await resp.query(req.body.query)
             info.result = result
         }
