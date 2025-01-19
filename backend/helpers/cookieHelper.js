ed4a48b9cf072151a584c2cf86b703bb3cb6b6f0
Zique <ezequielafeita@gmail.com>
2025-01-18 23:25:51 -0300

    Cookie

--- backend/helpers/cookieHelper.js
+++ backend/helpers/cookieHelper.js
@@ -1,5 +1,6 @@
 function cookieHelper(cookies) {
-    const cookie = cookies ? JSON.parse(decodeURIComponent(cookies.split("=")[1]).slice(2,-7)) : false;
+    console.log(cookies)
+    const cookie = cookies ? JSON.parse(decodeURIComponent(cookies.split("=")[1]).slice(2,35)) : false;
     return cookie   
 }
 
